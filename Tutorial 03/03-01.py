053bbda27f2c36e783d202bb005620a70f63a524
Hummingbird1986.local <danieldeng@me.com>
2026-01-30 23:58:20 +0800

    Add 03-01

--- Tutorial 03/03-01.py
+++ Tutorial 03/03-01.py
@@ -4,29 +4,29 @@ def find_valid_plates(text):
     result, prefix_number=[],[]
     match = r'\b([A-Z]{1,3})\s*([0-9]{1,4})\s*([A-Z]{1})\b'
     result = re.findall(match, text, re.IGNORECASE)
-    # print('result: ',result)
-    prefix_number=get_alphabet_mapping(result)
-    postfix_number=get_number_mapping(result)
+    results = [(item[0], item[1].lstrip('0'), item[2]) for item in result]
+    prefix_number=get_alphabet_mapping(results)
+    postfix_number=get_number_mapping(results)
     whole_number=[]
-    for i in range(len(result)):
+    for i in range(len(results)):
         whole_number.append(prefix_number[i][1]+postfix_number[i])
-
+    # print('results: ', results)
     # print('prefix_number: ',prefix_number)
     # print('postfix_number: ',postfix_number)
     # print(whole_number)
     # test_number_=0
     # checksum_=''
     valid_car_num=[]
-    for j in range(len(result)) :
+    for j in range(len(results)) :
         test_number_=check_sum_weights(whole_number[j])
         checksum_=checksum_letter(test_number_)
         # print(checksum_)
-        if checksum_==result[j][2]:
+        if checksum_==results[j][2]:
             # print(prefix_number[j][0])
-            # print(result[j][1])
-            valid_car_num.append(result[j][0].upper()+result[j][1]+result[j][2])
+            # print(results[j][1])
+            valid_car_num.append(results[j][0].upper()+results[j][1]+results[j][2])
     return valid_car_num
-            # print("true car number: ',postfix_number[0]+result[j][1])
+            # print("true car number: ',postfix_number[0]+results[j][1])
 
 
 
@@ -36,18 +36,18 @@ def get_alphabet_mapping(car_number_alpha):
     prefix_num_lst=[]
     for i in range(len(car_number_alpha)):
         alpha = car_number_alpha[i][0]
-        if alpha.isalpha():
-             if len(alpha) == 1:
-                 alpha='00'+alpha.upper()
-             elif len(alpha) == 2:
-                 alpha='0'+alpha.upper()
-             else:
-                 alpha=alpha.upper()
-             alpha_to_number_list = []
-             for j in range(1,len(alpha)):
-                alpha_to_number_list.append(alpha_to_number[alpha[j]])
-                prefix_number_=(alpha,alpha_to_number_list)
-             prefix_num_lst.append(prefix_number_)
+        # if alpha.isalpha():
+        if len(alpha) == 1:
+            alpha='00'+alpha.upper()
+        elif len(alpha) == 2:
+            alpha='0'+alpha.upper()
+        else:
+            alpha=alpha.upper()
+        alpha_to_number_list = []
+        for j in range(1,len(alpha)):
+           alpha_to_number_list.append(alpha_to_number[alpha[j]])
+           prefix_number_=(alpha,alpha_to_number_list)
+        prefix_num_lst.append(prefix_number_)
     return prefix_num_lst
 
 
@@ -56,18 +56,17 @@ def get_number_mapping(car_number_number):
     for i in range(len(car_number_number)):
         number_=car_number_number[i][1]
         number_list_=[]
-        if number_.isdigit():
-             for j in number_:
-                 number_list_.append(int(j))
-             if len(number_list_) == 1:
-                 number_list_ = [0, 0, 0] + number_list_
-             elif len(number_list_) == 2:
-                 number_list_ = [0, 0] + number_list_
-             elif len(number_list_) == 3:
-                 number_list_ = [0] + number_list_
-             else:
-                 pass
-             postfix_num_lst.append(number_list_)
+        for j in number_:
+            number_list_.append(int(j))
+        if len(number_list_) == 1:
+            number_list_ = [0, 0, 0] + number_list_
+        elif len(number_list_) == 2:
+            number_list_ = [0, 0] + number_list_
+        elif len(number_list_) == 3:
+            number_list_ = [0] + number_list_
+        else:
+            pass
+        postfix_num_lst.append(number_list_)
 
     return postfix_num_lst
 # mapping=get_alphabet_mapping()
@@ -86,4 +85,4 @@ def checksum_letter(check_sum_num):
     checksum='AZYXUTSRPMLKJHGEDCB'
     return checksum[i]
 
-print(find_valid_plates('Meet at carpark with plates: SGP1234A, sL 0123 Z, Gbf307K and S-12 A SG 5443K'))
+print(find_valid_plates('Meet at carpark with plates: SGP1234A, sL 0123 Z, Gbf0307K and S-12 A SG 5443K'))
