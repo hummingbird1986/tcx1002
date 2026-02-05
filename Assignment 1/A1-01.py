4cb1bd5da8a9814f48b47e49ce94fa3ee61bd8b6
Hummingbird1986.local <danieldeng@me.com>
2026-02-05 21:28:35 +0800

    Add A1-01 and A1-02

---
+++ Assignment 1/A1-01.py
@@ -0,0 +1,36 @@
+def min_workers(demand):
+   allocated_workers = []
+   demand_ = demand.copy()
+   population = 0
+   while True :
+        start_day_tuple = highest_demand_days(demand_)
+        start_day = start_day_tuple[0]
+        for offset in range(5):
+            idx = (start_day + offset ) % 7
+            # print(idx)
+            if demand_[idx]> 0:
+                demand_[idx] -= 1
+        allocated_workers.append(start_day)
+        population += 1
+        if highest_demand_days(demand_)[1]==0:
+             return  population,allocated_workers
+
+def highest_demand_days(demand):
+    max_val = -1
+    best_start_day = 0
+    for start_day in range(7):
+        current_window_sum = 0
+        for offset in range(5):
+            day = (start_day + offset) % 7
+            current_window_sum += demand[day]
+
+        if current_window_sum > max_val:
+            max_val = current_window_sum
+            best_start_day = start_day
+
+    return best_start_day, max_val
+print(min_workers([4, 2, 4, 3, 5, 4, 6]))
+# print(highest_demand_days([4, 2, 4, 3, 5, 4, 6]))
+#(7, [2, 3, 4, 2, 5, 0, 2])
+# print(min_workers([1, 1, 1, 1, 1, 2, 2])[0])
+#2
---
+++ Assignment 1/A1-02.py
@@ -0,0 +1,85 @@
+# 凡是在课程结束后才扫码的人员，均记为缺勤（0分）。
+# 1. 对于测验/评估课 (AS - Assessments):
+# 缺勤 (0分)：
+# 在课程进行到一半时间之后才到达。
+# 或者，在课程开始 1 小时之后才到达。
+# 迟到 (0.5分)：
+# 在课程开始后的 15 分钟至 1 小时内到达（即：过了 15 分钟但没过 1 小时）。
+# 出勤 (1分)：
+# 在课程开始后的 15 分钟之内到达。
+# 2. 对于讲座 (LT - Lectures) 和 辅导课 (TR - Tutorials):
+# 出勤 (1分)：只要学生在课程结束前完成扫码，即视为出勤。
+# 缺勤 (0分)：课程结束后才扫码或未扫码。
+from datetime import datetime, timedelta
+from operator import truediv
+
+from jupyter_server.auth import passwd
+
+sessions = {
+    1: ('LT', datetime(2025,1,1,19,0,0),  datetime(2025,1,1,21,0,0)),
+    2: ('TR', datetime(2025,1,8,10,30,0), datetime(2025,1,8,12,30,0)),
+    3: ('TR', datetime(2025,1,12,19,0,0), datetime(2025,1,12,20,0,0)),
+    4: ('LT', datetime(2025,1,15,19,0,0), datetime(2025,1,15,22,0,0)),
+    5: ('TR', datetime(2025,1,16,19,0,0), datetime(2025,1,16,20,0,0)),
+    10: ('AS', datetime(2025,1,21,19,0,0), datetime(2025,1,21,20,0,0)),
+    11: ('AS', datetime(2025,1,22,19,0,0), datetime(2025,1,22,21,0,0)),
+}
+
+qr_scans = [
+    (1, datetime(2025,1,1,19,1,0),  'Alice'),
+    (1, datetime(2025,1,1,19,8,0),  'Bob'),
+    (1, datetime(2025,1,1,19,35,0), 'Charlie'),
+    (2, datetime(2025,1,8,10,25,0), 'Alice'),
+]
+
+def status(session_type, start, end, qr_time):
+    duration_ = (end - start).total_seconds()/60
+    half_duration_ = duration_/2
+    late_time_= (qr_time-start).total_seconds()/60
+    # print(duration_,half_duration_,late_time_,session_type)
+
+    if session_type == 'AS':
+       if late_time_ >= 60.0 or late_time_ >= half_duration_:
+            score_=0.0
+       elif late_time_ <= 15.0:
+            score_=1.0
+       else:
+            score_=0.5
+    else:
+       if late_time_< duration_:
+            score_=1.0
+       else:
+            score_=0.0
+    return score_
+
+def attendance(query):
+    student_score={}
+    history=[]
+    if isinstance(query, datetime): #判断是否为 datetime 的类
+        return True
+        # return [(name, qr_time, score), (name, qr_time, score), ...]
+    elif isinstance(query, str): #判断是否为 String
+        for candidate in qr_scans:
+            student_name_=candidate[2]
+            if student_name_.upper()==query.upper():
+                session_id_=candidate[0]
+                qr_time_=candidate[1]
+                session_type_=sessions[session_id_][0].upper()
+                start_time_=sessions[session_id_][1]
+                end_time_=sessions[session_id_][2]
+                score_=status(session_type_,start_time_,end_time_,qr_time_)
+                history.append((session_id_,qr_time_,score_))
+                if student_name_ not in student_score:
+                    student_score[student_name_]=0.0
+                student_score[student_name_]+=score_
+                # print(f'score_= {score_:.1f} and session_id_= {session_id_}')
+                # print(student_score)
+        return student_score[student_name_],history
+        # return (total_score, [(session_id, qr_time, score), (session_id, qr_time, score)...])
+    else:
+        return False
+
+
+print(attendance('alice'))
+print(attendance('ALice')[0]== 2.0)
+print(attendance(datetime(2025,1,1,19,1,0   )
