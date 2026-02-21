1c4132b3b03388c38ab6e932399b011c64146d44
daniel <dnaieldeng@me.com>
2026-02-21 11:59:17 +0800

    Add mock_test_01.py

---
+++ mock_test_01.py
@@ -0,0 +1,17 @@
+def f(s):
+   
+    if s == '':
+        return ''
+    first_ele=s[0]
+    
+  
+    if first_ele.lower() in ['a', 'e', 'i', 'o', 'u']:
+           type = "V"
+    elif first_ele.isalpha() and first_ele.lower() not in ['a', 'e', 'i', 'o', 'u']:
+           type = "C"
+    else:
+           type = "-"
+           
+    return type + f(s[1:])
+
+print(f('e2cbi'))
